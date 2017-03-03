03cd3434dfe1a24233d4e0dcb2e24d4b122c7949
Andre Branchizio <andrejbranch@gmail.com>
2017-03-03 13:26:45 -0700

    removing unused files

--- src/common/admin/admin-group-detail-controller.js
+++ src/common/admin/admin-group-detail-controller.js
@@ -4,15 +4,11 @@ angular.module('admin.adminGroupDetailCtrl', [])
 
         function ($scope, group, groupFormFactory, users, roles) {
 
-
             $scope.group = group;
             $scope.users = users;
             $scope.roles = roles;
             $scope.edit = groupFormFactory.openFormModal;
 
-            $scope.groupTypeIconMapping = {
-
-            };
         }
 
     ])
