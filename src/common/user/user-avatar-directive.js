75f2f908256fdb361438e140bb0a5e3ba63aa3d9
Ross Troseth <hossthebossross@gmail.com>
2017-06-23 16:27:16 -0500

    files created

---
+++ src/common/user/user-avatar-directive.js
@@ -0,0 +1,38 @@
+angular.module('user.userAvatarDirective', [])
+    .directive('userAvatar', ['sessionFactory', 'API', '$rootScope',
+        function (sessionFactory, API, $rootScope) {
+            return {
+                restrict: 'E',
+                templateUrl: 'common/user/user-avatar-tpl.html',
+                link: function ($scope) {
+
+                    $scope.getUser = function () {
+
+                        return sessionFactory.getLoggedInUser();
+
+                    }
+
+                    $scope.hasAvatar = function () {
+
+                        return typeof $scope.getUser().avatarAttachment !== 'undefined';
+
+                    }
+
+                    $scope.setAvatarSrc = function () {
+
+                        $scope.avatarSrc = $scope.hasAvatar()
+                            ? API.url + '/attachment/' + $scope.getUser().avatarAttachment.id + '/download'
+                            : null
+                        ;
+
+                    }
+
+                    $scope.setAvatarSrc();
+
+                    $rootScope.$on('profile.avatar_upload', $scope.setAvatarSrc);
+
+                }
+            }
+        }
+    ])
+;
