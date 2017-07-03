03a92e7a5ab8573ab1e9fc196659d3190ee56fc6
Ross Troseth <hossthebossross@gmail.com>
2017-07-03 13:48:50 -0500

    first pass admin pass reset

--- src/common/admin/admin-user-detail-controller.js
+++ src/common/admin/admin-user-detail-controller.js
@@ -1,13 +1,31 @@
 angular.module('admin.adminUserDetailCtrl', [])
 
-    .controller('adminUserDetailCtrl', ['$scope', 'user', 'userFormFactory', 'groups',
+    .controller('adminUserDetailCtrl', ['$scope', 'user', 'userFormFactory', 'groups', '$uibModal',
 
-        function ($scope, user, userFormFactory, groups) {
+        function ($scope, user, userFormFactory, groups, $modal) {
 
             $scope.user = user;
             $scope.groups = groups;
             $scope.edit = userFormFactory.openFormModal;
 
+            $scope.changePassword = function () {
+                $modal.open({
+                    templateUrl: 'common/user/partials/user-change-password-tpl.html',
+                    controller: 'adminResetPasswordCtrl',
+                    windowClass: 'inmodal',
+                    keyboard: false,
+                    backdrop: 'static',
+                    resolve: {
+
+                        user: function () {
+
+                            return $scope.user;
+
+                        }
+
+                    }
+                });
+            }
         }
 
     ])
