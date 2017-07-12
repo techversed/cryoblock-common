93be5f8b5cef6da9d8c83f2fac5e691b14cebad1
yashasri <yash@yashasris-mbp.dhcp.mc.vanderbilt.edu>
2017-07-12 13:27:15 -0500

    password changed &enable button

--- src/common/admin/admin-user-detail-controller.js
+++ src/common/admin/admin-user-detail-controller.js
@@ -1,12 +1,28 @@
 angular.module('admin.adminUserDetailCtrl', [])
 
-    .controller('adminUserDetailCtrl', ['$scope', 'user', 'userFormFactory', 'groups', '$uibModal',
+    .controller('adminUserDetailCtrl', ['$scope', 'user', 'userFormFactory', 'groups', '$uibModal','sessionFactory',
 
-        function ($scope, user, userFormFactory, groups, $modal) {
+        function ($scope, user, userFormFactory, groups, $modal, sessionFactory) {
 
             $scope.user = user;
             $scope.groups = groups;
             $scope.edit = userFormFactory.openFormModal;
+
+            $scope.canDisable = true;
+            if ($scope.user.id === sessionFactory.getLoggedInUser().id) {
+                $scope.canDisable = false;
+
+            }
+            if (!$scope.user.enabled) {
+                $scope.canDisable = false;
+            }
+
+
+            $scope.disable = function (user) {
+
+                userFormFactory.openDisableModal(user);
+
+            };
 
             $scope.changePassword = function () {
                 $modal.open({
