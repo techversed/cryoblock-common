40e3c9c0159815c8dbe1b7aeda720686ac5e2f75
uyashasri <yashasri.umareddy@vanderbilt.edu>
2017-09-29 15:27:46 -0500

    method not working

--- src/common/storage/storage-flag-division-form-controller.js
+++ src/common/storage/storage-flag-division-form-controller.js
@@ -1,10 +1,43 @@
 angular.module('storage.storageFlagDivisionFormCtrl', [])
 
-    .controller('storageFlagDivisionFormCtrl', ['$uibModal', 'storageFactory', '$state', '$stateParams', '$cbGridBuilder', '$cbResource',
+    .controller('storageFlagDivisionFormCtrl', ['$scope', '$uibModalInstance', 'division', '$cbResource', 'toastr',
 
-        function ($modal, storageFactory, $state, $stateParams, $cbGridBuilder, $cbResource) {
+        function ($scope, $modalInstance, division, $cbResource, toastr) {
+
+            $scope.division = division;
 
 
+            $scope.close = function () {
+                $modalInstance.close();
+            };
+
+            $scope.submit = function (isValid) {
+
+                $scope.$broadcast('form:submit');
+
+                $scope.submitted = true;
+
+                if (!isValid) {
+                    return;
+                }
+
+                var url = '/storage/flag?id[EQ]=' + $scope.division.id;
+
+                $cbResource[method](url, $scope.division).then(
+
+                    function (response) {
+
+                        toastr.info('Group ' + method + 'd successfully');
+                        $scope.close();
+                        callback();
+                    },
+
+                    function (response) {
+
+                        $scope.errors = response.data;
+                    }
+                );
+            }
         }
     ])
 ;
