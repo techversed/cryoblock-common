96e47f00c98cafede9c64777f0a344165455beea
Taylor Jones <tjonesster@gmail.com>
2018-12-10 12:21:28 -0600

    renamed catalog controller to catalog index controller to be in line with the naming convention -- also added window.scroll(0,0) to a number of controllers to fix the issue that we had where it would load to the same spot on a page as it was on the previous page -- this is just one of the side effects of having a single page app

--- src/common/grid/filter/grid-filter-promise-sharer.js
+++ src/common/grid/filter/grid-filter-promise-sharer.js
@@ -1,29 +1,36 @@
 angular.module('grid.gridFilterPromiseSharer' , [])
     .service('gridFilterPromiseSharer', ['$cbResource',
 
+        /*
+            If mutiple grids in the same page are requesting the same list this service can be used in order to ensure that only one request needs to be made for that resource.
+            This works by maintainging a list of all currently oustanding promoises and allows for multiple callback functions to be registered to the same promise.
+            This is only currently used by the grid-relation-filter-controller in order to only have to fetch one copy of the starting menu items for each relation filter.
+
+            Written by Taylor Jones.
+
+        */
+
         function ($cbResource) {
 
             var promiseGrouper =  {
 
-                // Store all of our promises in this
                 promiseMap: {},
 
-                // If a promise does not exist, create it if it does exist then register another callback.
                 addPromise: function (url, params) {
-                    //addPromise
                     var that = this;
+                    var parTesting = $cbResource.serializeParams(params)
 
-                    if (that.promiseMap[url+params] == undefined) {
+                    if (that.promiseMap[url+parTesting] == undefined) {
 
-                        that.promiseMap[url+params] = $cbResource.get(url, params);
+                        that.promiseMap[url+parTesting] = $cbResource.get(url, params);
 
-                        that.promiseMap[url+params].then(function (response) {
-                            that.promiseMap[url+params] = undefined;
+                        that.promiseMap[url+parTesting].then(function (response) {
+                            that.promiseMap[url+parTesting] = undefined;
                         });
 
                     }
 
-                    return that.promiseMap[url+params].then(function (response) {
+                    return that.promiseMap[url+parTesting].then(function (response) {
                         return response;
                     });
                 }
