8cc5be6806b1ce84aee4e38ffe31eb355c6c9a94
Taylor Jones <tjonesster@gmail.com>
2019-01-22 10:21:25 -0600

    set up the profile picture to show the correct profile of the person who's page you go to

--- src/common/profile/profile-controller.js
+++ src/common/profile/profile-controller.js
@@ -5,29 +5,35 @@ angular.module('profile.profileCtrl', [])
 
             $scope.user = user;
             $scope.grid = grid;
-            $scope.userBool = userBool;
+            console.log("returning user bool");
+            console.log(userBool);
+            $scope.userBool = userBool; // User Bool true of the user is going to their own profile page -- false if not--
 
             // $scope.bool =
 
             $scope.edit = profileFormFactory.openFormModal;
 
             $scope.hasAvatar = function () {
-                return typeof $scope.user.avatar_attachment !== 'undefined';
+                return typeof $scope.user.avatarAttachment !== 'undefined';
             }
 
             $scope.avatarSrc = $scope.hasAvatar()
-                ? API.url + '/attachment/' + $scope.user.avatar_attachment.id + '/download'
+                ? API.url + '/attachment/' + $scope.user.avatarAttachment.id + '/download'
                 : null
             ;
 
+            // Don't allow for people to pop the upload photo modal on someone else's profile page
             $scope.uploadPhoto = function () {
-                $modal.open({
-                    templateUrl: 'common/profile/partials/profile-photo-upload-tpl.html',
-                    controller: 'photoUploadCtrl',
-                    windowClass: 'inmodal',
-                    keyboard: false,
-                    backdrop: 'static'
-                });
+                console.log('upload photo function');
+                if (userBool) {
+                    $modal.open({
+                        templateUrl: 'common/profile/partials/profile-photo-upload-tpl.html',
+                        controller: 'photoUploadCtrl',
+                        windowClass: 'inmodal',
+                        keyboard: false,
+                        backdrop: 'static'
+                    });
+                }
             }
 
             $scope.changePassword = function () {
