0331515b1312532f4af3ae248c956be58728c797
Taylor Jones <tjonesster@gmail.com>
2019-01-28 14:12:39 -0600

    removed the comment routes thing -- that would take longer than I am willing to invest into it -- we just need to ship this release and we will have to put up with that not being in it if we want to get it out the door

--- src/common/comment/comment.js
+++ src/common/comment/comment.js
@@ -2,6 +2,5 @@ angular.module('comment', [
     'comment.cbObjectCommentsDirective',
     'comment.commentFactory',
     'comment.cbCommentDirective',
-    'comment.cbCommentDeleteCtrl',
-    'comment.routes'
+    'comment.cbCommentDeleteCtrl'
 ]);
