8f414238063ab956b186b00bdd5c1790dce101cc
Taylor Jones <tjonesster@gmail.com>
2019-02-18 13:06:35 -0600

    the gridform is starting to take shape

--- src/common/grid-form/grid-form-column/type/single-relation/grid-form-single-relation-column-directive.js
+++ src/common/grid-form/grid-form-column/type/single-relation/grid-form-single-relation-column-directive.js
@@ -1,16 +1,17 @@
 angular.module('gridForm.gridFormColumn.gridFormSingleRelationColumnDirective', [])
-
     .directive('gridFormSingleRelationColumn', [
 
+        /*
+            Need to add a way to create a new a new entry if a name does not exist.
+            Allow the user to select one of a certain type of object.
+            Boolean to allow for the creation of new objects.
+
+
+            column -- bind to
+
+        */
+
         function () {
-
-            /*
-
-                Need to add a way to create a new a new entry if a name does not exist.
-                Allow the user to select one of a certain type of object.
-                Boolean to allow for the creation of new objects.
-
-            */
 
             return {
 
@@ -25,8 +26,10 @@ angular.module('gridForm.gridFormColumn.gridFormSingleRelationColumnDirective', 
                 controller: function ($scope) {
 
                     var init = function () {
-                        $scope.catalog = {};
-                        $scope.sample = {'catalog': {}};
+                        // $scope.catalog = {};
+                        // $scope.sample = {'catalog': {}};
+                        console.log("running the init funciton in the grid form single relation column directive");
+                        $scope.suggestionList = ['asdf1', 'asdf2', 'asdf3', 'asdf4', 'asdf5'];
 
                     };
 
--- src/common/grid-form/grid-form-column/type/single-relation/grid-from-single-relation-column-factory.js
+++ src/common/grid-form/grid-form-column/type/single-relation/grid-from-single-relation-column-factory.js
@@ -1,14 +1,35 @@
 angular.module('gridForm.gridFormColumn.gridFormSingleRelationColumnFactory', [])
     .factory('gridFormSingleRelationColumnFactory', [
 
+        /*
+            This factory creates an object which holds all of the information pertaining to single realations
+
+        */
+
         function () {
+
+            // Boolean to allow for the creation of new elements
+
+
 
             var gridFormSingleRelationColumn = {
 
                 url: "asdf",
-
+                selectedElement: [],
+                elementsToShow: [],
+                numberOfElementsToFetch: [],
+                allowCreation: true
 
             };
+
+
+            /*
+                function setSelection
+                function getItems
+                function setItems
+                function selectElement
+
+            */
 
             return gridFormSingleRelationColumn;
 
