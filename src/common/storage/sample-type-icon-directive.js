69ef69675d56ea111d3fda75f7d55f71d72a3a61
Taylor Jones <tjonesster@gmail.com>
2019-03-05 15:33:45 -0600

    fixed some small icon stuff

--- src/common/storage/sample-type-icon-directive.js
+++ src/common/storage/sample-type-icon-directive.js
@@ -1,5 +1,11 @@
 angular.module('storage.sampleTypeIconDirective', [])
 
+    /*
+        TAYLOR-- This directive assumes that 'sampleTypeIconMapping' is defined in the local branch that implements this...
+        It would probably be a good idea to at least have the default icon mapping located in common... happy with whatever.
+
+    */
+
     .directive('sampleTypeIcon', ['sampleTypeIconMapping',
 
         function (sampleTypeIconMapping) {
