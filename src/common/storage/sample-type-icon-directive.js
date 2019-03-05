af718f506185efa08ec3745d5d03b5a9242f57f7
Taylor Jones <tjonesster@gmail.com>
2019-03-05 14:40:16 -0600

    small styling changes

--- src/common/storage/sample-type-icon-directive.js
+++ src/common/storage/sample-type-icon-directive.js
@@ -19,12 +19,14 @@ angular.module('storage.sampleTypeIconDirective', [])
 
                     $scope.setIconSrc = function () {
                         var mapping;
+
                         if ($scope.sampleTypeIconMapping[$scope.sampleType.name]==undefined) {
                                 mapping = $scope.sampleTypeIconMapping["default"];
                         }
                         else {
                             mapping = $scope.sampleTypeIconMapping[$scope.sampleType.name];
                         }
+
                         $scope.iconSrc = '/images/' + mapping;
 
                     }
