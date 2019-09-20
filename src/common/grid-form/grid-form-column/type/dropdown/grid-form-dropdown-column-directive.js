fa212162899f7b596917232cc5fb7b1bd8769ede
Taylor Jones <tjonesster@gmail.com>
2019-09-20 14:18:27 -0500

    clean up comments from gridforms

--- src/common/grid-form/grid-form-column/type/dropdown/grid-form-dropdown-column-directive.js
+++ src/common/grid-form/grid-form-column/type/dropdown/grid-form-dropdown-column-directive.js
@@ -33,7 +33,7 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
                     var getIndex = function (needle, haystack) {
 
                         for (i=0; i < haystack.length; i++) {
-                            if (haystack[i]==needle) return i;
+                            if (haystack[i] == needle) return i;
                         }
 
                         return -1;
@@ -44,7 +44,7 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
 
                         var tmp = [];
 
-                        for(i=0; i<count;i++){
+                        for (i=0; i<count;i++) {
                             tmp.push(value);
                         }
 
@@ -55,19 +55,9 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
                     var init = function () {
 
                         $scope.minSelectable
-
                         $scope.obj[$scope.field] = $scope.obj[$scope.field] ? $scope.obj[$scope.field] : [];
-
                         $scope.selectMultiple = $scope.column.maxSelectable ? false : ($scope.column.maxSelectable == 1)  ? true : true;
-
-
-                        // In the end this is going to end up querying the backend.
-                        // console.log("column", $scope.column);
-                        // $scope.suggestionList = ['asdf1', 'asdf2', 'asdf3', 'asdf4', 'asdf5'];
-                        // console.log("obj", $scope.obj);
-
                         $scope.suggestionList = $scope.column.acceptedValues;
-
                         $scope.highlightedElement = $scope.suggestionList[0];
 
                         $scope.selectedThing = {};
@@ -87,10 +77,8 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
 
                     $scope.keyPressHandler = function (event, item) {
 
-                        // console.log(event);
 
                         if (event.key == "Enter") {
-                            // console.log(event);
 
                             $scope.selectItem($scope.highlightedElement);
 
@@ -98,29 +86,17 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
 
                         else if (event.key == "ArrowDown") {
 
-                            // console.log("event", event);
-
-                            // Should move onto the next item in the list if there is one
-                            // console.log("pressed Arrow down");
-                            // var index = $scope.suggestionList.find( findFunction(element, index, $scope.highlightedElement) );
-
                             var index = getIndex($scope.highlightedElement, $scope.suggestionList);
 
                             if (index+1 < $scope.suggestionList.length) {
                                 $scope.highlightedElement = $scope.suggestionList[index+1];
                             }
 
-                            // event.stopPropagation();
-                            // event.detail.keyboardEvent.preventDefault()
-
                             event.preventDefault();
-                            // console.log("should have stopped propagation");
 
                         }
 
                         else if (event.key == "ArrowUp") {
-
-                            // console.log("pressed Arrow up");
 
                             var index = getIndex($scope.highlightedElement, $scope.suggestionList);
 
@@ -130,28 +106,22 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
 
                             event.preventDefault();
 
-                            // console.log("should have stopped propagation");
                         }
 
                         // Don't move down the page when space is pressed
                         else if (event.key == " ") {
 
-                            // console.log("pressed space");
                             event.preventDefault();
 
                         }
 
                         else if (event.key == "Backspace") {
 
-                            // console.log("pressed");
+                            console.log("May not even need anything to handle backspace");
 
                         }
 
                         else if (event.key == 'a' && event.metaKey == true) {
-
-                            console.log(event);
-                            console.log("selection start", event.currentTarget.selectionStart);
-                            console.log("selection End", event.currentTarget.selectionEnd);
 
                             var sel = window.getSelection();
                             var el = event.currentTarget;
@@ -160,11 +130,7 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
                             sel.removeAllRanges();
                             sel.addRange(range);
 
-                            // event.currentTarget.selectionStart = 0;
-                            // event.currentTarget.selectionEnd = 10;
-
                             event.preventDefault();
-                            // If the user hits ctrl / cmd + a then we want to keep the selection within the currently selected cell.
 
                         }
 
@@ -185,14 +151,12 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
                                 }
                             }
                             $scope.obj[$scope.field] = tmpList;
-                            // $scope.selectionListString = $scope.selectedValues.join(", ");
 
                         }
 
                         else {
 
-                            if ($scope.obj[$scope.field].includes(item))
-                            {
+                            if ($scope.obj[$scope.field].includes(item)) {
                                 $scope.obj[$scope.field] = [];
                             }
                             else {
@@ -212,45 +176,34 @@ angular.module('gridForm.gridFormColumn.gridFormDropdownColumnDirective', [])
 
                     $scope.shiftFocus = function () {
 
-                        console.log("This should now shift the focus to the search bar located within this directive");
-
                         $scope.focusGained = true;
-
-                        // document.getElementById("testing").focus();
-                        // I don't know whether or not we are even going to end up doing the whole searchbar thing for this afterall -- still to be determined...
+                        // console.log("shifting focus is not implemented yet I don't think");
 
                     };
 
                     $scope.catchPaste = function (event) {
 
-
-                        console.log(event);
-                        console.log(event.originalEvent.clipboardData.getData('text'));
-
                         var tmp = event.originalEvent.clipboardData.getData('text').trim().split(', ');
-
-                        console.log("tmp", tmp);
 
                         $scope.focusGained = false;
 
                     };
 
                     $scope.catchCopy = function (event) {
-                        console.log("caught the copy event");
 
                         $scope.focusGained = false; // If they copy, close the
+
                     };
 
                     $scope.catchCut = function (event) {
-                        console.log("caught the copy event");
 
                         $scope.focusGained = false; // If they copy, close the
+
                     };
 
                     $scope.unselectItem = function (thing) {
 
                         $scope.selectItem(thing);
-                        console.log(thing);
 
                     };
 
