7d43833173cb4f61fa670d6076d4050bdcc02066
Taylor Jones <tjonesster@gmail.com>
2020-02-27 15:26:44 -0600

    removed some comments

--- src/common/grid-form/grid-form-factory.js
+++ src/common/grid-form/grid-form-factory.js
@@ -41,29 +41,6 @@ angular.module('gridForm.gridFormFactory', [])
 
                 // We are going to move towards setting this through the url instead
 
-                // Removing some error checking bullshit
-                // [
-                //     {id: "id", header: "Text", field: "Field1", defVal: "default value for that column when a new object is created", required: true, fieldName: "asdf", type: "text", validators: ["list of functions"], sortingOperator: function () {}, sortable:true},
-                //     {id: "id", header: "Dropdown", field: "Field2", required: true, fieldName: "asdf", type: "dropdown", validInputs: ["list", "of", "possible", "inputs"], validators: ["List of functions"]},
-                //     {id: "id", header: "Single-Relation", field: "Field3", required: true, fieldName: "asdf", type: "relation", backendUrl: 'path to url', validators: ["list of functions"]},
-                //     {id: "id", header: "Multi-Relation", field: "Field4", required: true, fieldName: "asdf", type: "relation", minrequired: "Min number of linked objects for it to be valid", maxrequired: "Max number of linked objects for it to be a valid input", backendUrl: 'path to url', validators: ["List of functions"]},
-                // ];
-
-                // Removing some error checking bullshit
-                // Row Definitions
-                // [
-                //     {id: 0, collapsed: false, deleted: false, entity: {'Field1': 'asdf0', 'Field2': [], 'Field3': [], 'Field4': []}},
-                //     {id: 1, collapsed: false, deleted: false, entity: {'Field1': 'asdf1', 'Field2': [], 'Field3': [], 'Field4': []}},
-                //     {id: 2, collapsed: false, deleted: false, entity: {'Field1': 'asdf2', 'Field2': [], 'Field3': [], 'Field4': []}},
-                //     {id: 3, collapsed: false, deleted: false, entity: {'Field1': 'asdf3', 'Field2': [], 'Field3': [], 'Field4': []}},
-                //     {id: 4, collapsed: false, deleted: false, entity: {'Field1': 'asdf4', 'Field2': [], 'Field3': [], 'Field4': []}}
-                // ];
-                    // {id: "id", header: "Boolean", field: "Field5", defVal:true, required: true, fieldName: "asdf", type: "boolean", backendUrl: 'path to url', validators: ["List of functions"]},
-
-                // Want to make it possible to group rows in the best way possible. -- could set it up to group by sample id for instance.
-                // this.rowOrdering = [0,1,2,3,4];
-                // this.refreshUrl = null;
-
                 this.rows = [];
                 this.columns = [];
                 this.rowOrdering = [];
@@ -81,7 +58,6 @@ angular.module('gridForm.gridFormFactory', [])
                     return this.columnns;
 
                 },
-
 
                 setColumns: function (columns) {
 
@@ -122,7 +98,6 @@ angular.module('gridForm.gridFormFactory', [])
 
                 },
 
-
                 // Takes a list of objects and adds a row for each one -- might be a good idea to copy them bofore adding them so that you don't change the initial object
                 addRowsFromObjects: function (ents) {
 
@@ -138,13 +113,11 @@ angular.module('gridForm.gridFormFactory', [])
 
                 },
 
-
                 // Add an element to the gridform
                 addEmptyRow: function () {
 
                     // For each column set default value for that field.
                     // Set to empty
-
                     // {id: 4, collapsed: false, deleted: false, entity: {'Field1': 'asdf4', 'Field2': [], 'Field3': [], 'Field4': []}}
 
                     var emptyRow = {};
@@ -162,50 +135,34 @@ angular.module('gridForm.gridFormFactory', [])
 
                     this.addRowFromObject(emptyRow);
 
-
-
                 },
 
                 validate: function() {
 
-
                     console.log("Run the validation stuff -- to be written on the backend");
 
-
                 },
-
-
 
                 // We will implement this sutf later.
                 // This should take a template and a controller and make it so that the given row actions were all possible on the current gridform.
                 addRowActions: function (templatePath, controller) {
 
                     // asdf
-
                 },
-
 
                 // Create a list of updates and a list of creations -- send to backend
                 collectUpdates: function () {
-
                     // Collect updates, creations, deletions
-
                 },
-
 
                 // Remove an element from the grid form
                 removeRow: function (row) {
-
                     // asdf
-
-
                 },
 
 
                 refresh: function () {
-
                     // asdf
-
                 }
 
 
