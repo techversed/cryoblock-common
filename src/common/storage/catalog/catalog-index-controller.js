967aed602da917bf4c9211633ae47bf5643c6311
Ross Troseth <hossthebossross@gmail.com>
2021-08-10 22:24:09 -0500

    catalog-links

--- src/common/storage/catalog/catalog-index-controller.js
+++ src/common/storage/catalog/catalog-index-controller.js
@@ -8,6 +8,27 @@ angular.module('storage.catalog.catalogIndexCtrl', [])
 
             $scope.grid = grid;
 
+            $scope.linky = function () {
+                window.open(
+                    'https://crowelab-utilities.service.vumc.org/#/target/index',
+                    '_blank' // <- This is what makes it open in a new window.
+                );
+            };
+
+            $scope.linky2 = function () {
+                window.open(
+                    'https://crowelab-utilities.service.vumc.org/#/donor/donor/index',
+                    '_blank' // <- This is what makes it open in a new window.
+                );
+            };
+
+            $scope.linky3 = function () {
+                window.open(
+                    'https://crowelab-utilities.service.vumc.org/#/production/human-specimen-request/index',
+                    '_blank' // <- This is what makes it open in a new window.
+                );
+            };
+
         }
 
     ])
